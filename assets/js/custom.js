6632f522ffecd64502ca3ca19bb38c874cd1f342
Robert Davis <me@robertdavis.co.uk>
2019-01-21 21:38:02 +0000

    Adding dynamic tags per project

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,11 +3,21 @@ $(function() {
 	//Variables
 	var londonUrl = "embed?pb=!1m18!1m12!1m3!1d158858.47339377046!2d-0.24169925079317647!3d51.52855824344214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon!5e0!3m2!1sen!2suk!4v1501607670852"
 	var lincolnUrl = "embed?pb=!1m18!1m12!1m3!1d38223.60525201374!2d-0.59051569876191!3d53.21829071772276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48780ccb711104fb%3A0x36603dcfe8c7ddc7!2sLincoln!5e0!3m2!1sen!2suk!4v1501607748018"
-	var ytTwo = "https://www.youtube.com/embed/N1kqBtfwzhA";
-	var ytThree = "https://www.youtube.com/embed/bJvG35SPK6w";
-	var titleOne = "Group Project: VR Blacksmith Simulator";
-	var titleTwo = "Dissertation: Using Virtual Reality for Education";
-	var titleThree = "VR Time Crisis Prototype";
+	
+	var projectOneTitle = "Group Project: VR Blacksmith Simulator";
+	var projectOneYoutubeVideo = "";
+	var projectOneDescription = "Undergraduate research project, developed in Unity with C# amoung a group of 4. The project allows the player to experience traditional blacksmithing in Virtual Reality and lets them create and save their creations.";
+	var projectOneTags = ["C#", "Unity", "Group Project", "Virtual Reality"];
+
+	var projectTwoTitle = "Dissertation: Using Virtual Reality for Education";
+	var projectTwoYoutubeVideo = "https://www.youtube.com/embed/N1kqBtfwzhA";
+	var projectTwoDescription = "My dissertation project, created in Unreal Engine 4 using C++.";
+	var projectTwoTags = ["C++", "Unreal Engine", "Linear", "Scripting", "Virtual Reality"];
+
+	var projectThreeTitle = "Gameplay Prototype: VR Time Crisis";
+	var projectThreeYoutubeVideo = "https://www.youtube.com/embed/bJvG35SPK6w";
+	var projectThreeDescription = "A Virtual Reality gameplay prototype developed in Unreal Engine using C++ in the style of Time Crisis. After eliminating the enemies each round, the player advances through the level automatically.";
+	var projectThreeTags = ["C++", "Gameplay Prototype", "Unreal Engine", "Virtual Reality"];
 
 	var linkLondon = $("#linkLondon");
 	var linkLincoln = $("#linkLincoln");
@@ -21,6 +31,7 @@ $(function() {
 	var linkPortBoxTwo = $("#two");
 	var linkPortBoxThree = $("#three");
 	var projectOverlayExit = $(".project-overlay-exit");
+	var projectTags = $("#tags");
 
 
 	function expandMap(locationURL) { 
@@ -64,13 +75,13 @@ $(function() {
 		
 		switch(projectRef) {
 			case 1:
-				fillProjectContent(titleOne, ytThree);
+				fillProjectContent(projectOneTitle, projectThreeYoutubeVideo, projectOneDescription, projectOneTags);
 				break;
 			case 2:
-				fillProjectContent(titleTwo, ytTwo);
+				fillProjectContent(projectTwoTitle, projectTwoYoutubeVideo, projectTwoDescription, projectTwoTags);
 				break;
 			case 3:
-				fillProjectContent(titleThree, ytThree);
+				fillProjectContent(projectThreeTitle, projectThreeYoutubeVideo, projectThreeDescription, projectThreeTags);
 				break;
 			default:
 				console.log("Error with youtube link");
@@ -84,10 +95,19 @@ $(function() {
 		isPortBoxExpanded = false;
 	};
 
-	function fillProjectContent(projectTitle, youtubeLink) {
+	function fillProjectContent(projectTitle, projectYoutubeLink, projectDescription, projectTagsArray) {
 		$("#project-title").html(projectTitle);
-		$("#youtube_video").attr('src', youtubeLink);
-		//$(".tags").empty();
+		$("#project-video").attr('src', projectYoutubeLink);
+		$("#project-description").text(projectDescription);
+		
+		// Empty the tags and refill them for each project
+		projectTags.empty();
+		for(var i = 0; i < projectTagsArray.length; i++) 
+		{
+			// Add each of the tags
+			projectTags.append("<li>" + projectTagsArray[i] + "</li>");
+			console.log(projectTagsArray[i]);
+		}
 	}
 
 	linkPortBoxOne.click(function() { 
